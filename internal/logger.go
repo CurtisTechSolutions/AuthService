61955c5e062fefde6842876180ea26839aa7e45a
Mason <nosam1998@gmail.com>
2025-05-12 12:40:17 -0700

    feat: Refactor logger initialization to use a global logger instance

--- internal/logger.go
+++ internal/logger.go
@@ -5,10 +5,27 @@ import (
 	"os"
 )
 
+var logger *TLogger
+
+type TLogger struct {
+	Logger *slog.Logger
+}
+
 // InitializeLogger sets up the logger for the application.
 // It configures the logger to output JSON formatted logs to stdout
 func InitializeLogger(options *slog.HandlerOptions) {
 	// Set up logger
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, options))
-	slog.SetDefault(logger)
+	logger = &TLogger{
+		Logger: slog.New(slog.NewJSONHandler(os.Stdout, options)),
+	}
+}
+
+func GetLogger() *slog.Logger {
+	if logger == nil {
+		InitializeLogger(&slog.HandlerOptions{
+			AddSource: true,
+			Level:     slog.LevelInfo,
+		})
+	}
+	return logger.Logger
 }
